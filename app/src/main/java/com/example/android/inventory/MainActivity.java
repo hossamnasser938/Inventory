4918f17e1f9a655b4752dc7ae67a208b002672b3
Hosam <hossmnasser938@gmail.com>
2018-04-29 01:48:49 +0200

    Changes to meet requirements

--- app/src/main/java/com/example/android/inventory/MainActivity.java
+++ app/src/main/java/com/example/android/inventory/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.example.android.inventory.data.InventoryContract.Product;
 import com.example.android.inventory.data.InventoryDBHelper;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,30 +23,52 @@ public class MainActivity extends AppCompatActivity {
         InventoryDBHelper helper = new InventoryDBHelper(this);
         SQLiteDatabase db = helper.getReadableDatabase();
 
-        InventoryDBHelper.insertRowIntoInventoryTable(db);
+        InventoryDBHelper.insertRowIntoInventoryTable(getApplicationContext(), db);
 
         Cursor c = InventoryDBHelper.readDataFromInventoryTable(db);
-
-        String dataRetrieved = "";
-        for(int i = 0, j = c.getColumnCount(); i < j; i++){
-            dataRetrieved += c.getColumnName(i);
-            if(i != j - 1){
-                dataRetrieved += " | ";
-            }
-        }
-
-        dataRetrieved += "\n";
-
-        while(c.moveToNext()){
+        try {
+            //Display the table on the screen
+            String dataRetrieved = "";
             for(int i = 0, j = c.getColumnCount(); i < j; i++){
-                dataRetrieved += c.getString(i);
+                dataRetrieved += c.getColumnName(i);
                 if(i != j - 1){
                     dataRetrieved += " | ";
                 }
             }
+
             dataRetrieved += "\n";
+
+            int idColumnIndex = c.getColumnIndex(Product._ID);
+            int productNameColumnIndex = c.getColumnIndex(Product.COLUMN_PRODUCT_NAME);
+            int priceColumnIndex = c.getColumnIndex(Product.COLUMN_PRICE);
+            int quantityColumnIndex = c.getColumnIndex(Product.COLUMN_QUANTITY);
+            int supplierNameColumnIndex = c.getColumnIndex(Product.COLUMN_SUPPLIER_NAME);
+            int supplierPhoneNumberColumnIndex = c.getColumnIndex(Product.COLUMN_SUPPLIER_PHONE_NUMBER);
+
+            int currentId;
+            String currentProductName;
+            double currentPrice;
+            int currentQuantity;
+            String currentSupplierName;
+            String currentSupplierPhoneNumber;
+
+            while(c.moveToNext()){
+                currentId = c.getInt(idColumnIndex);
+                currentProductName = c.getString(productNameColumnIndex);
+                currentPrice = c.getDouble(priceColumnIndex);
+                currentQuantity = c.getInt(quantityColumnIndex);
+                currentSupplierName = c.getString(supplierNameColumnIndex);
+                currentSupplierPhoneNumber = c.getString(supplierPhoneNumberColumnIndex);
+
+                dataRetrieved += "" + currentId + " | " + currentProductName + " | " + currentPrice + " | " + currentQuantity + " | " + currentSupplierName + " | " + currentSupplierPhoneNumber + "\n";
+            }
+
+            mainText.setText(dataRetrieved);
+        }
+        finally {
+            //Get rid of the Cursor object
+            c.close();;
         }
 
-        mainText.setText(dataRetrieved);
     }
 }
--- app/src/main/java/com/example/android/inventory/data/InventoryDBHelper.java
+++ app/src/main/java/com/example/android/inventory/data/InventoryDBHelper.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
 import com.example.android.inventory.data.InventoryContract.Product;
 
 /**
@@ -25,7 +27,7 @@ public class InventoryDBHelper extends SQLiteOpenHelper {
         //Create table Product SQL statement
         String SQL_CREATE_TABLE_PRODUCT = "CREATE TABLE" + Product.TABLE_NAME + "("
                 + Product._ID + InventoryContract.DATA_INTEGER + InventoryContract.CONSTRAINT_PRIMARY_KEY + InventoryContract.CONSTRAINT_AUTOINCREMENT + ", "
-                + Product.COLUMN_PRODUCT_NAME + InventoryContract.DATA_TEXT + InventoryContract.CONSTRAINT_NOT_NULL + InventoryContract.CONSTRAINT_UNIQUE + ", "
+                + Product.COLUMN_PRODUCT_NAME + InventoryContract.DATA_TEXT + InventoryContract.CONSTRAINT_NOT_NULL + ", "
                 + Product.COLUMN_PRICE + InventoryContract.DATA_REAL + ", "
                 + Product.COLUMN_QUANTITY + InventoryContract.DATA_INTEGER + InventoryContract.CONSTRAINT_DEFAULT + " 0" + ", "
                 + Product.COLUMN_SUPPLIER_NAME + InventoryContract.DATA_TEXT + ", "
@@ -40,7 +42,7 @@ public class InventoryDBHelper extends SQLiteOpenHelper {
         //Nothing for now
     }
 
-    public static void insertRowIntoInventoryTable(SQLiteDatabase db){
+    public static void insertRowIntoInventoryTable(Context context, SQLiteDatabase db){
         //Prepare Product info
         String productName = "mouse", supplierName = "Hosam", supplierPhoneNumber = "01026501235";
         double price = 15.0;
@@ -55,7 +57,13 @@ public class InventoryDBHelper extends SQLiteOpenHelper {
         values.put(Product.COLUMN_QUANTITY, quantity);
 
         //Insert this row in Product table
-        db.insert(Product.TABLE_NAME, null, values);
+        if(db.insert(Product.TABLE_NAME, null, values) != -1){
+            Toast.makeText(context, "Item Inserted", Toast.LENGTH_SHORT);
+        }
+        else {
+            Toast.makeText(context, "Error Inserting Item", Toast.LENGTH_SHORT);
+        }
+
     }
 
     public static Cursor readDataFromInventoryTable(SQLiteDatabase db){
